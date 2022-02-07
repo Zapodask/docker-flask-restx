30a267e9c95f53892a918af89527f31917424a81
Zapodask <joaopedroemerim@gmail.com>
2022-02-07 00:56:25 -0300

    nginx

--- api/index.py
+++ api/index.py
@@ -6,6 +6,6 @@ from src.app import app
 if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
-        port=os.getenv("PORT"),
+        port=5000,
         debug=True if os.getenv("STAGE") == "dev" else False,
     )
